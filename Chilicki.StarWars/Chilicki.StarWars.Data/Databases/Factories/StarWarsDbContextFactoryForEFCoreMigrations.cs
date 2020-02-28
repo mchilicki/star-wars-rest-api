f3bc6edc42ae2f741d3bec837c9cde88186c38ac
mchilicki <marcin.chilicki96@gmail.com>
2020-02-28 16:04:02 +0100

    Add Azure deployment and simplify database connection string configuration

--- Chilicki.StarWars/Chilicki.StarWars.Data/Databases/Factories/StarWarsDbContextFactoryForEFCoreMigrations.cs
+++ Chilicki.StarWars/Chilicki.StarWars.Data/Databases/Factories/StarWarsDbContextFactoryForEFCoreMigrations.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Chilicki.StarWars.Data.Databases.Factories
@@ -10,13 +12,26 @@ namespace Chilicki.StarWars.Data.Databases.Factories
     {
         public StarWarsDbContext CreateDbContext(string[] args)
         {
-            var databaseConnectionString = "data source=localhost;initial catalog=StarWarsDb;integrated security=True;MultipleActiveResultSets=True;";
+            var configuration = GetConfiguration();
+            var databaseConnectionString = configuration.GetConnectionString("StarWars");
             var optionsBuilder = new DbContextOptionsBuilder<StarWarsDbContext>();
             optionsBuilder.UseSqlServer(
                 databaseConnectionString,
                 b => b.MigrationsAssembly(typeof(StarWarsDbContext).Assembly.GetName().Name)
             );
             return new StarWarsDbContext(optionsBuilder.Options);
+        }
+
+        private IConfigurationRoot GetConfiguration()
+        {
+            var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile($"appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{environmentName}.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
+            var configuration = builder.Build();
+            return configuration;
         }
     }
 }
